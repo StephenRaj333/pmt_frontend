a1d82ffdf5e7bb5d4e4c908aaf94d73ccedafe76
Stephen Raj <130592637+StephenRaj333@users.noreply.github.com>
2025-02-06 23:43:26 +0530

    Update _document.tsx

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -4,7 +4,9 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
-<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
+<meta
+            name="viewport"
+            content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" /> 
 </Head>
       <body className="antialiased">
         <Main />
